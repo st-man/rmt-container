bafdebf1265744efbb1dbc97ff302d8a970e91c2
st-man <92167612+st-man@users.noreply.github.com>
2022-09-09 14:23:52 +0300

    Update certgen.sh

--- certgen.sh
+++ certgen.sh
@@ -2,9 +2,9 @@
 # Install 
 ## Create  RMT Certificates
 #### Change hostname and ip_address to your data.
-export CA_PWD="rmt"
+export CA_PWD="_rmt"
 export hostname=$HOSTNAME.sslip.io
-export ip_address=192.168.1.135
+export ip_address=[192.168.1.1]
 openssl genrsa -aes256 -passout env:CA_PWD -out ./ssl/rmt-ca.key 2048
 openssl req -x509 -new -nodes -key ./ssl/rmt-ca.key -sha256 -days 1825 -out ./ssl/rmt-ca.crt -passin env:CA_PWD -config ./ssl/rmt-ca.cnf
 openssl genrsa -out ./ssl/rmt-server.key 2048
