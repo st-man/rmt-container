4dc5df146aea527ffa1807821d92f8e07095c431
st-man <92167612+st-man@users.noreply.github.com>
2022-09-18 12:08:47 +0300

    Update rmt-start.sh

--- rmt-start.sh
+++ rmt-start.sh
@@ -63,7 +63,7 @@ echo -e "database:\n  host: ${MYSQL_HOST}\n  database: ${MYSQL_DATABASE}\n  user
 echo -e "  adapter: mysql2\n  encoding: utf8\n  timeout: 5000\n  pool: 5\n" >> /etc/rmt.conf
 echo -e "scc:\n  username: ${SCC_USERNAME}\n  password:  ${SCC_PASSWORD}\n  sync_systems: true\n" >> /etc/rmt.conf
 echo -e "log_level:\n  rails: info" >> /etc/rmt.conf
-sed -i 's/RSYNC_USER/${RSYNC_USER}/g ; s/RMT_REMOTE_HOST/${RMT_REMOTE_HOST}/g' /var/spool/cron/tabs/root
+sed -i 's/RSYNC_USER/${RSYNC_USER}/g; s/RMT_REMOTE_HOST/${RMT_REMOTE_HOST}/g; s/DATE/$(date +"%d-%m-%Y")/g' /var/spool/cron/tabs/root
 
 
 if [ $# -eq 0 ]; then
